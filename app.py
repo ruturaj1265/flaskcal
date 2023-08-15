683811e483052ffb1e696c5fd3408ba0794fbbee
Ruturaj Chavan <ruturajchavan1265@gmail.com>
2023-08-15 14:24:19 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,request,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return "Welcome to the flask"
+
+print(__name__)
+
+@app.route('/calc',method = ['GET'])
+def calc():
+    operation = request.json("operation")
+    number1 = request.json("number1")
+    number2 = request.json("number2")
+    if operation=="add":
+        result = number1+number2
+    elif operation=="multiply":
+        result = number1*number2
+    elif operation == "division":
+        result = number1/number2
+    else:
+        result = number1 - number2
+    return result
+
+if __name__ == "__main__":
+    app.run()
